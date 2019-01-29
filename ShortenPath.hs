880b16f90efc850fb63e6206c72011a8014d13a1
Matthew Todd <matthew_git@zerobitcoder.net>
2019-01-29 10:52:37 -0800

    Adding license information to ShortenPath.hs

--- ShortenPath.hs
+++ ShortenPath.hs
@@ -4,6 +4,22 @@
 -- @date: 2019-01-29
 --
 --
+-- Copyright 2019 Matthew Todd
+-- 
+-- This file is part of Shorten Path.
+-- 
+-- Shorten Path is free software: you can redistribute it and/or modify it
+-- under the terms of the GNU General Public License as published by the Free
+-- Software Foundation, either version 3 of the License, or (at your option)
+-- any later version.
+-- 
+-- Shorten Path is distributed in the hope that it will be useful, but WITHOUT
+-- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+-- FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+-- more details.
+-- 
+-- You should have received a copy of the GNU General Public License along with
+-- Shorten Path.  If not, see <http://www.gnu.org/licenses/>.
 
 {-# LANGUAGE CPP #-}
 
